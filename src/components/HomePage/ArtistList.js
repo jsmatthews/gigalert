225e4b4366c379f0a0f8207b1f5d0416b3f6f63c
James <jsmatthews89@gmail.com>
2017-12-22 19:39:39 +0900

    Converted HomePage to a container

---
+++ src/components/HomePage/ArtistList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import ArtistLink from '../Artists/ArtistLink'
+import { Link } from 'react-router-dom'
+
+import '../../styles/HomePage.css'
+
+export default class ArtistList extends Component {
+
+    listPopularArtists() {
+        return this.props.artists.map(artist => {
+            return (
+                <Link key={artist.id} to={`/artists/${artist.id}`}>
+                    <ArtistLink {...artist} />
+                </Link>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className='page home-page'>
+                {this.listPopularArtists()}
+            </div>
+        )
+    }
+}
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,33 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 
 import { fetchArtists } from '../../actions/index'
-import ArtistLink from '../Artists/ArtistLink'
+
+import ArtistList from './ArtistList'
 
 import '../../styles/HomePage.css'
 
 class HomePage extends Component {
+
     componentDidMount() {
         this.props.dispatch(fetchArtists())
     }
 
-    listPopularArtists() {
-        return this.props.artists.map(artist => {
-            return (
-                <Link key={artist.id} to={`/artists/${artist.id}`}>
-                    <ArtistLink {...artist} />
-                </Link>
-            )
-        })
-    }
-
     render() {
-        return (
-            <div className='page home-page'>
-                {this.listPopularArtists()}
-            </div>
-        )
+        return <ArtistList artists={this.props.artists} />
     }
 }
 
